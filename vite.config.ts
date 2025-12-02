f51b5d70ccd55227f55d34c521f675df0c36ae2a
fannzxyl <alfanjanaur20@contoh.com>
2025-12-02 12:15:39 +0700

    beta Self-Checkout 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // WAJIB untuk Vercel static
       server: {
         port: 3000,
         host: '0.0.0.0',
